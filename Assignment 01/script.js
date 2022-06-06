ab99c91dddcbcf87469efee9a67b55fc2df07a6b
Yunchan94 <juleefang94@gmail.com>
2022-06-06 22:42:47 +0700

    selector va submit button

---
+++ Assignment 01/script.js
@@ -0,0 +1,34 @@
+"use strict";
+// Selector
+const idInput = document.getElementById("input-id");
+const nameInput = document.getElementById("input-name");
+const ageInput = document.getElementById("input-age");
+const typeInput = document.getElementById("input-type");
+const weightInput = document.getElementById("input-weight");
+const lengthInput = document.getElementById("input-length");
+const colorInput = document.getElementById("input-color-1");
+const breedInput = document.getElementById("input-breed");
+const vaccinatedInput = document.getElementById("input-vaccinated");
+const dewormedInput = document.getElementById("input-dewormed");
+const sterilizedInput = document.getElementById("input-sterilized");
+const submitBtn = document.getElementById("submit-btn");
+const healthyBtn = document.getElementById("healthy-btn");
+const deleteBtn = document.getElementById("btn-danger");
+
+// Submit
+submitBtn.addEventListener("click", function () {
+  let data = {
+    id: idInput.value,
+    name: nameInput.value,
+    age: parseInt(ageInput.value),
+    type: typeInput.value,
+    weight: parseInt(weightInput.value),
+    length: parseInt(lengthInput.value),
+    color: colorInput.value,
+    breed: breedInput.value,
+    vaccinated: vaccinatedInput.checked,
+    dewormed: dewormedInput.checked,
+    sterilized: sterilizedInput.checked,
+    date: new Date(),
+  };
+});
